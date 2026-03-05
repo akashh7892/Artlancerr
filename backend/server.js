dff1b8a944715315241061ebc5d083e293b3deff
gagansv <gagansv8@gmail.com>
2026-03-05 09:41:45 +0530

    added message for root / in bavkend

--- backend/server.js
+++ backend/server.js
@@ -77,6 +77,11 @@ app.use(
 app.use(express.json());
 app.use(mongoSanitize());
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/artist", artistRoutes);
@@ -163,3 +168,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
